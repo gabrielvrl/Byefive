f897e64af539b9eaf7c88f27b5804e097c0846a3
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-24 15:19:37 -0300

    Header component built

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -4,13 +4,37 @@ import "./Header.css";
 import logo from "../../assets/logoByefiveFull.png";
 
 function Header({ history }) {
+    function handleAboutUs(e) {
+        e.preventDefault();
+        history.push("/aboutus");
+    }
+
+    function handleContact(e) {
+        e.preventDefault();
+        history.push("/contact");
+    }
+
+    function handleLogin(e) {
+        e.preventDefault();
+        history.push("/login");
+    }
+
     return (
-        <div className="headerDiv">
-            <div className="imgDiv" onClick={() => history.push("/")}>
-                <img src={logo} alt="Byefive" />
+        <nav>
+            <div className="headerDiv">
+                <div className="leftSection">
+                    <div className="imgDiv" onClick={() => history.push("/")}>
+                        <img src={logo} alt="Byefive" />
+                    </div>
+                    <h1 onClick={handleAboutUs}>Sobre Nós</h1>
+                    <h1 onClick={handleContact}>Contato</h1>
+                </div>
+                <div className="rightSection">
+                    <h1 onClick={handleLogin}>Login</h1>
+                    <button id="nav-learn-more-button">Quero saber mais</button>
+                </div>
             </div>
-            <h1>Login</h1>
-        </div>
+        </nav>
     );
 }
 
