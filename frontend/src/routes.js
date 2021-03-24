c5a4d27a9aac7a97c32fded29f3ca42d74219708
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-24 16:43:12 -0300

    routes update

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 export default function Routes() {
     return (
@@ -10,6 +12,8 @@ export default function Routes() {
             <Header />
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/login" exact component={Login} />
+                <Route path="/signup" exact component={SignUp} />
             </Switch>
         </BrowserRouter>
     );
