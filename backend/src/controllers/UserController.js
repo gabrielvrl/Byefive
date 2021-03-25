036e5a38c565c4ecb2f8d3174201436a66ce625d
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-25 11:39:01 -0300

    update controller

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,15 +1,12 @@
-const User = require("../models/user");
+const User = require("../models/User");
 
 module.exports = {
     async store(req, res) {
-        const { username, password, firstName, lastName } = req.body;
-        console.log(req.body.cargo);
+        const { firstName, lastName, email, username, password } = req.body;
+        console.log(req.body);
 
         const userExists = await User.findOne({
             username: username,
-            password: password,
-            firstName: firstName,
-            lastName: lastName,
         });
 
         if (userExists) {
@@ -18,10 +15,11 @@ module.exports = {
 
         //API? const response = await axios.get(`link da api/${username}`);
         const user = await User.create({
-            username,
-            password,
             firstName,
             lastName,
+            email,
+            username,
+            password,
         });
 
         return res.json({ user });
