bc521ebcab101d1649893acb3f847ea4778d9fcc
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-26 14:41:21 -0300

    Home component update

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,7 +1,45 @@
 import React from "react";
+import "./Home.css";
 
-function Home() {
-    return <div>Home</div>;
+import personagem from "../../assets/Personagem-Com-Celular.png";
+
+function Home({ history }) {
+    function handleLogin(e) {
+        e.preventDefault();
+        history.push("/login");
+    }
+
+    function handleSignup(e) {
+        e.preventDefault();
+        history.push("/signup");
+    }
+
+    return (
+        <div className="container-home">
+            <div className="text-content">
+                <h1 id="firstP">Melhore sua gestão de</h1>
+                <h1> Promotoras & Pontos de Venda</h1>
+                <h2>
+                    Agora você controla bônus, comissões e cada venda do seu PDV
+                    em tempo real. Conecte-se com promotoras e vendedoras de um
+                    jeito mais inteligente.
+                </h2>
+                <h2 id="login" onClick={handleLogin}>
+                    Faça o <b>Login</b>
+                </h2>
+                <h2 id="login" onClick={handleSignup}>
+                    Ainda não tem cadastrado? <b>Cadastre-se!</b>
+                </h2>
+            </div>
+            <div className="img-content">
+                <img
+                    className="personagem-img"
+                    src={personagem}
+                    alt="Personagem com Celular"
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Home;
