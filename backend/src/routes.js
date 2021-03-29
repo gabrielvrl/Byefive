a34b202cb5b3c69066a8a0959fc3a10985d08fbf
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-03-29 13:55:28 -0300

    removing comment

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,11 +3,6 @@ const UserController = require("./controllers/UserController");
 
 const routes = express.Router();
 
-// passo o endereco que ela vai, exemplo: localhost:3333/vaiaqui
-//routes.get('/' , (req, res) => {
-//    return res.send('opa world')
-//});
-
 routes.post("/signupuser", UserController.store);
 
 module.exports = routes;
